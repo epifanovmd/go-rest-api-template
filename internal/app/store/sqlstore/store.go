d4ffd57330abaafb1594102ed31f1f5597dab6f8
Epifanov <epifanovmd@gmail.com>
2021-03-22 11:08:11 +0200

    first commit

---
+++ internal/app/store/sqlstore/store.go
@@ -0,0 +1,32 @@
+package sqlstore
+
+import (
+	"database/sql"
+	"go-rest-api-template/internal/app/store"
+)
+
+// Store ...
+type Store struct {
+	db             *sql.DB
+	userRepository *UserRepository
+}
+
+// New ...
+func New(db *sql.DB) *Store {
+	return &Store{
+		db: db,
+	}
+}
+
+// User ...
+func (s *Store) User() store.UserRepository {
+	if s.userRepository != nil {
+		return s.userRepository
+	}
+
+	s.userRepository = &UserRepository{
+		store: s,
+	}
+
+	return s.userRepository
+}
